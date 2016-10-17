f82b3516c6e6a4df5ace21c10822c0af66f779df
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-10-17 09:37:15 -0400

    Changed installation directory for aparapi.

--- aparapi.sh
+++ aparapi.sh
@@ -1,2 +1,2 @@
-export LIBAPARAPI=/opt/AMDAPP/aparapi/lib
-export LD_LIBRARY_PATH=/opt/AMDAPP/aparapi/lib:$LD_LIBRARY_PATH
+export LIBAPARAPI=/opt/aparapi/lib
+export LD_LIBRARY_PATH=/opt/aparapi/lib:$LD_LIBRARY_PATH
